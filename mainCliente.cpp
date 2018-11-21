9fd23ab11283fc897a6f14558c281857817a87e8
AbrahamGarcia240 <zildjianremo@gmail.com>
2018-11-20 20:15:30 -0600

    Envio y borro con hilos una linea

--- mainCliente.cpp
+++ mainCliente.cpp
@@ -2,12 +2,21 @@
 #include <iostream>
 #include <string>
 #include <map> 
+#include <thread>
 #include "Solicitud.h"
 #include "mensaje.h"
+#include "Semaforo.h"
 using namespace std;
 
+Semaforo sem1, sem2;
 map <int, double> coordenadas;
-
+mensaje prueba;
+double datos;
+int numeroSecuenciaRecibido;
+int n=1;
+int numeroSecuencia=1;  
+     
+char *resultado=(char*)malloc(sizeof(TAM_MAX_DATA));
 void CreaCoordenadas(int n){
 	register int i;
 	for (i=1; i<=n; i++){
@@ -29,30 +38,10 @@ mensaje FormarMensaje(int numeroSecuencia, int entrada, string ip, int puerto, i
 	return prueba;
 }
 
-int main(int argc, char const *argv[])
-{	
-	if(argc<3){
-		cout<<"Modo de uso ./mainCliente <ip_cliente> <ip_servidor>"<<endl;
-		return 0;
-	}
-	int entrada;
-	cout<<"Ingrese el monto a ingresar"<<endl;
-	cin>>entrada;
-	CreaCoordenadas(entrada);
-	std::string ip(argv[1]); //mi IP
-	std::string ip2(argv[2]); //IP DEL SERVIDOR
-	
-
-	mensaje prueba;
-	Solicitud cliente(ip);
-	double datos;
-	int numeroSecuenciaRecibido;
-	int n=1;
-	int numeroSecuencia=1;  
-	int aux=1;      
-	char *resultado=(char*)malloc(sizeof(TAM_MAX_DATA));
-	int entradaAux=entrada;
-
+void funcion1(string ip, string ip2, int entrada, Solicitud cliente)
+{
+	int aux=1;
+	sem1.wait();
 	while(entrada>0){
 		
 		prueba=FormarMensaje(numeroSecuencia,entrada,ip,7777,1,(double)aux,(double)coordenadas.find(numeroSecuencia)->second);
@@ -77,8 +66,13 @@ int main(int argc, char const *argv[])
 		
 		
 	}
-	aux=1;
-	while(entradaAux>0){
+	sem2.post();
+}
+
+void funcion2(string ip, string ip2, int entrada, Solicitud cliente){
+	int aux=1;
+	sem2.wait();
+	while(entrada>0){
 		
 		prueba=FormarMensaje(numeroSecuencia,entrada,ip,7777,2,(double)aux,(double)coordenadas.find(numeroSecuencia)->second);
 		
@@ -96,12 +90,41 @@ int main(int argc, char const *argv[])
 				cout<<datos<<endl;
 				numeroSecuencia++;
 				aux++;
-				entradaAux--;
+				entrada--;
 			
 		}
 		
 		
 	}
+	sem1.post();
+}
+
+int main(int argc, char const *argv[])
+{	
+
+	
+
+	if(argc<3){
+		cout<<"Modo de uso ./mainCliente <ip_cliente> <ip_servidor>"<<endl;
+		return 0;
+	}
+	int entrada;
+	cout<<"Ingrese el monto a ingresar"<<endl;
+	cin>>entrada;
+	CreaCoordenadas(entrada);
+	std::string ip(argv[1]); //mi IP
+	std::string ip2(argv[2]); //IP DEL SERVIDOR
+	Solicitud cliente(ip);
+	sem1.init(1);
+	sem2.init(0);
+	thread th1(funcion1,ip,ip2,entrada,cliente), th2(funcion2, ip, ip2, entrada,cliente);
+
+	th1.join();
+	th2.join();
+
+	
+	
+	
 
 	
 	return 0;
